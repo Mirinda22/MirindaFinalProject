4603e36fd74e557f3a9f0d89700170f6ca6ad1fc
Bryan Chu <bryan.p.chu@gmail.com>
2015-11-28 14:28:18 -0500

    inital flexbox setup

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+/* CURRENTLY IN: javascript/main.js */
+// Initialize collapse button
+  // $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
